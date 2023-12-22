904daa6ec93601d7400207bbbde9d35b7ebf8648
Hugo Ramoz Andrade <hugoramoz37@gmail.com>
2023-12-22 00:32:24 -0300

    Diretorio que contém scripts

---
+++ Script/Retirando-backup-s3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+CAMINHO_RESTORE=/home/(usuário)/restore_amazon
+aws s3 sync s3://[nome do bucket]/$(date +%F) $CAMINHO_RESTORE
+
+
+cd $CAMINHO_RESTORE
+if [ -f $1.sql ]
+then
+    mysql -u root mutillidae < $1.sql
+    if [ $? -eq 0 ]
+    then
+        echo "O restore foi realizado com sucesso"
+    fi
+else
+    echo "O arquivo procurado não existe no diretorio"
+fi
